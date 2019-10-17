187a6179987c9a34188dec1df5707038a3ba5202
Samir Habibi <56111263+Shabibii@users.noreply.github.com>
2019-10-17 17:57:24 +0100

    GUI of Registration page added. Custom controls which are needed for this page added as well.

---
+++ Software_Engineering_Assignment/CustomControls/StaffRegistrationControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engineering_Assignment.CustomControls
+{
+    public partial class StaffRegistrationControl : UserControl
+    {
+        public StaffRegistrationControl()
+        {
+            InitializeComponent();
+        }
+
+        //Registeration field conttrol
+        /*
+         * Get doctors from database
+         * create many regFControls for the doctors
+         * Add controls to panel (with auto scroll(scroll bar))
+         */
+    }
+}
---
+++ Software_Engineering_Assignment/CustomControls/UnavailableStaffControl.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Software_Engineering_Assignment.CustomControls
+{
+    partial class UnavailableStaffControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(269, 6);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unavailable Staff (+ End Date Unavailability)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 249);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Dr Samuel Segi (12 Oct 2019)\r\nDr Josephine Luke (09 Oct 2019)\r\n……..\r\n……..\r\n……..\r\n" +
+    "……..\r\n……..\r\n……..\r\n……..\r\n……..\r\n……..";
+            // 
+            // UnavailableStaff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "UnavailableStaff";
+            this.Size = new System.Drawing.Size(500, 291);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
