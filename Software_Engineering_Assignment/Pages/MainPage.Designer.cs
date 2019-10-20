75cd7ced2b4913bfc96331c6ee4deec8cf82dc36
franciscooliveira36 <56388595+franciscooliveira36@users.noreply.github.com>
2019-10-20 13:32:36 +0100

    bedside page first design

--- Software_Engineering_Assignment/Pages/MainPage.Designer.cs
+++ Software_Engineering_Assignment/Pages/MainPage.Designer.cs
@@ -40,9 +40,10 @@
             // 
             this.dateTimeLabel.AutoSize = true;
             this.dateTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimeLabel.Location = new System.Drawing.Point(11, 13);
+            this.dateTimeLabel.Location = new System.Drawing.Point(15, 16);
+            this.dateTimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dateTimeLabel.Name = "dateTimeLabel";
-            this.dateTimeLabel.Size = new System.Drawing.Size(372, 26);
+            this.dateTimeLabel.Size = new System.Drawing.Size(463, 31);
             this.dateTimeLabel.TabIndex = 3;
             this.dateTimeLabel.Text = "Saturday, 26 Oct 2019 (09:21:55 PM)";
             // 
@@ -53,19 +54,21 @@
             // register1
             // 
             this.register1.BackColor = System.Drawing.Color.White;
-            this.register1.Location = new System.Drawing.Point(543, 49);
-            this.register1.MaximumSize = new System.Drawing.Size(475, 557);
+            this.register1.Location = new System.Drawing.Point(724, 60);
+            this.register1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.register1.MaximumSize = new System.Drawing.Size(633, 686);
             this.register1.Name = "register1";
-            this.register1.Size = new System.Drawing.Size(464, 538);
+            this.register1.Size = new System.Drawing.Size(619, 662);
             this.register1.TabIndex = 2;
             // 
             // bayControl2
             // 
             this.bayControl2.BackColor = System.Drawing.Color.White;
             this.bayControl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.bayControl2.Location = new System.Drawing.Point(7, 322);
+            this.bayControl2.Location = new System.Drawing.Point(9, 396);
+            this.bayControl2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.bayControl2.Name = "bayControl2";
-            this.bayControl2.Size = new System.Drawing.Size(497, 265);
+            this.bayControl2.Size = new System.Drawing.Size(662, 326);
             this.bayControl2.TabIndex = 1;
             this.bayControl2.Click += new System.EventHandler(this.BayControl2_Click);
             // 
@@ -73,23 +76,26 @@
             // 
             this.bayControl1.BackColor = System.Drawing.Color.White;
             this.bayControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.bayControl1.Location = new System.Drawing.Point(7, 49);
+            this.bayControl1.Location = new System.Drawing.Point(9, 60);
+            this.bayControl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.bayControl1.Name = "bayControl1";
-            this.bayControl1.Size = new System.Drawing.Size(497, 265);
+            this.bayControl1.Size = new System.Drawing.Size(662, 326);
             this.bayControl1.TabIndex = 0;
+            this.bayControl1.Load += new System.EventHandler(this.BayControl1_Load);
             this.bayControl1.Click += new System.EventHandler(this.BayControl1_Click);
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.dateTimeLabel);
             this.Controls.Add(this.register1);
             this.Controls.Add(this.bayControl2);
             this.Controls.Add(this.bayControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainPage";
-            this.Size = new System.Drawing.Size(1010, 595);
+            this.Size = new System.Drawing.Size(1347, 732);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Software_Engineering_Assignment/Pages/PatientPage.Designer.cs
+++ Software_Engineering_Assignment/Pages/PatientPage.Designer.cs
@@ -28,19 +28,297 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // RegistrationPage
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.button1.Location = new System.Drawing.Point(754, 549);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 33);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(885, 549);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 33);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.Text = "Module 1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(372, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 24;
+            this.textBox2.Text = "2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(372, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 25;
+            this.textBox3.Text = "3";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(372, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 22);
+            this.textBox4.TabIndex = 26;
+            this.textBox4.Text = "4";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(372, 220);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 22);
+            this.textBox5.TabIndex = 27;
+            this.textBox5.Text = "5";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(775, 50);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(130, 22);
+            this.textBox6.TabIndex = 28;
+            this.textBox6.Text = "Module 2";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(775, 100);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(130, 22);
+            this.textBox7.TabIndex = 29;
+            this.textBox7.Text = "7";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(775, 140);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(130, 22);
+            this.textBox8.TabIndex = 30;
+            this.textBox8.Text = "8";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(775, 180);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(130, 22);
+            this.textBox9.TabIndex = 31;
+            this.textBox9.Text = "9";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(775, 220);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(130, 22);
+            this.textBox10.TabIndex = 32;
+            this.textBox10.Text = "10";
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(372, 320);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(130, 22);
+            this.textBox11.TabIndex = 33;
+            this.textBox11.Text = "Module 3";
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(372, 370);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(130, 22);
+            this.textBox12.TabIndex = 34;
+            this.textBox12.Text = "12";
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(372, 410);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(130, 22);
+            this.textBox13.TabIndex = 35;
+            this.textBox13.Text = "13";
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(372, 450);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(130, 22);
+            this.textBox14.TabIndex = 36;
+            this.textBox14.Text = "14";
+            this.textBox14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(372, 490);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(130, 22);
+            this.textBox15.TabIndex = 37;
+            this.textBox15.Text = "15";
+            this.textBox15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(775, 320);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(130, 22);
+            this.textBox16.TabIndex = 38;
+            this.textBox16.Text = "Module 4";
+            this.textBox16.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(775, 370);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(130, 22);
+            this.textBox17.TabIndex = 39;
+            this.textBox17.Text = "17";
+            this.textBox17.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(775, 410);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(130, 22);
+            this.textBox18.TabIndex = 40;
+            this.textBox18.Text = "18";
+            this.textBox18.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(775, 450);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(130, 22);
+            this.textBox19.TabIndex = 41;
+            this.textBox19.Text = "19";
+            this.textBox19.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(775, 490);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(130, 22);
+            this.textBox20.TabIndex = 42;
+            this.textBox20.Text = "20";
+            this.textBox20.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PatientPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Name = "RegistrationPage";
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PatientPage";
             this.Size = new System.Drawing.Size(1010, 595);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox20;
     }
 }
