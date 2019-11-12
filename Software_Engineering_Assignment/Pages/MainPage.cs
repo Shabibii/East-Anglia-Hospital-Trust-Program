0636b3c277c8bac35c1ee10a4575a273653acc81
sinbaddoraji <sinbaddoraji@gmail.com>
2019-11-12 22:35:27 +0000

    Registeration page login

--- Software_Engineering_Assignment/Pages/MainPage.cs
+++ Software_Engineering_Assignment/Pages/MainPage.cs
@@ -15,6 +15,7 @@ namespace Software_Engineering_Assignment.Pages
     {
         public Main.PageCall1 BayPageCall;
         public Main.PageCall0 ManagementPageCall;
+        public Main.PageCall0 RegisterationPageCall;
 
         public MainPage(Main.PageCall2 patientPageCall)
         {
@@ -76,10 +77,24 @@ namespace Software_Engineering_Assignment.Pages
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            Login login = new Login();
-            if (login.ShowDialog() == DialogResult.Yes)
+            using (Login login = new Login())
             {
                 //If login button clicked on the login form
+                if (login.ShowDialog() == DialogResult.Yes)
+                {
+                    Staff staff = login.Staff;
+
+                    if (staff == null)
+                    {
+                        //If invalid login
+                        MessageBox.Show("Invalid Login", "Invalid login", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        //if valid login of any staff type
+                        RegisterationPageCall();
+                    }
+                }
             }
         }
 
